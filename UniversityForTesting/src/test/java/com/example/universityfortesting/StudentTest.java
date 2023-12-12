c37f4314e60bc89a9461c20fb239a479e4df2289
Nick Whitelegg <nickw4426@gmail.com>
2023-12-12 16:02:36 +0100

    Fix testing for week 9

--- UniversityForTesting/src/test/java/com/example/universityfortesting/StudentTest.java
+++ UniversityForTesting/src/test/java/com/example/universityfortesting/StudentTest.java
@@ -1,3 +1,5 @@
+package com.example.universityfortesting;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,15 +12,47 @@
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+
 
 import com.example.universityfortesting.Student;
+import com.example.universityfortesting.University;
 
 public class StudentTest {
+    
+    private University university;
+    Student student1, student2;
+    static University university2;
+    
+    @BeforeEach
+    public void initUniversity() {
+        university = new University("The University of Romsey");
+        student1 = new Student("1", "Fred", "SE");
+        student2 = new Student("2", "Jane", "Networks");
+        university.enrolStudent(student1);
+        university.enrolStudent(student2);
+    }
+    
+    @BeforeAll
+    public static void initBeforeAll() {
+        university2 = new University("The University of Lyndhurst");
+        Student student1 = new Student("1", "Fred", "SE");
+        Student student2 = new Student("2", "Jane", "Networks");
+        university2.enrolStudent(student1);
+        university2.enrolStudent(student2);
+    }
     
     @Test
     public void testMark() {
         Student s = new Student("1", "Tom", "Computing");
         s.setMark(70.0);
         assertEquals(s.getGrade(), "First");
+    }
+    
+    @Test
+    public void testFindStudent() {
+       
+        assertEquals(university2.findStudentById("1").getName(), "Fred");
     }
 }
--- Week9Solution/src/test/java/com/example/week9solution/StudentDaoTest.java
+++ Week9Solution/src/test/java/com/example/week9solution/StudentDaoTest.java
@@ -6,6 +6,9 @@ package com.example.week9solution;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+
 import java.sql.*;
 
 /**
@@ -14,14 +17,37 @@ import java.sql.*;
  */
 public class StudentDaoTest {
 
-    @Test
-    public void testFindStudentById() throws Exception {
+    static Connection conn = null;
 
-        Connection conn = null;
-
+    @BeforeAll
+    public static void setupDb() {
         try {
             Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection("jdbc:sqlite:/home/nick/university.db");
+            StudentDao dao = new StudentDao(conn, "student");
+            Student s = new Student(0, "Charles Smith", "Chemistry", "undergraduate");
+            long id = dao.addStudent(s);
+            assertNotNull(dao.findStudentById(id));
+        } catch(SQLException | ClassNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+    
+    @AfterAll
+    public static void teardownDb() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    @Test
+    public void testFindStudentById() throws Exception {
+
+        try {
             StudentDao dao = new StudentDao(conn, "student");
             Student s = new Student(0, "Charles Smith", "Chemistry", "undergraduate");
             long id = dao.addStudent(s);
@@ -35,20 +61,10 @@ public class StudentDaoTest {
 
     @Test
     public void testNonExistentId() throws Exception {
-        Connection conn = null;
 
-        try {
-
-            Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:/home/nick/university.db");
-            StudentDao dao = new StudentDao(conn, "student");
-            Student s = new Student(0, "Charles Jones", "Computing", "undergraduate");
-            long id = dao.addStudent(s);
-            assertNull(dao.findStudentById(id + 1)); // will be null as the auto-allocated ID is the greatest so far
-        } finally {
-            if (conn != null) {
-                conn.close();
-            }
-        }
+        StudentDao dao = new StudentDao(conn, "student");
+        Student s = new Student(0, "Charles Jones", "Computing", "undergraduate");
+        long id = dao.addStudent(s);
+        assertNull(dao.findStudentById(id + 1)); // will be null as the auto-allocated ID is the greatest so far
     }
 }
